745cd6b2d2270673a155df10b64abb842e7bc142
Delbert <delbert.densu@hva.nl>
2024-10-04 09:21:11 +0200

    added logic

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,22 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        int counter = 1;
+
+        do {
+            String printout = Integer.toString(counter);
+
+            if (counter % 3 == 0) {
+                printout += "Brrr";
+            }
+            if (counter % 7 == 0) {
+                printout += "Pfft";
+            }
+
+            System.out.println(printout);
+            counter++;
+        } while (counter != 100);
     }
 }
